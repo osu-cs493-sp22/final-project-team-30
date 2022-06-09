5dcc821a0c3dac4b474b0cd7e2bed4764440defd
Benjiman Walsh <walshb@oregonstate.edu>
2022-06-09 12:51:42 -0700

    Added an admin user to the initialization script

--- initMongo.js
+++ initMongo.js
@@ -10,5 +10,21 @@ db.createUser({
    ]
 });
 
+db = db.getSiblingDB('tarpaulin')
+
+db.createCollection('users')
+
+
+db.users.insertMany([
+{
+   name: "Benjiman Walsh",
+   email: "walshb@oregonstate.edu",
+   password: "$2a$08$vOohJ7bOICkIn24gpv40j.Xz81EDOy.84eU1FpLXobP1jIEIkSrve",
+   role: "admin"
+
+}
+
+])
+
 
 
